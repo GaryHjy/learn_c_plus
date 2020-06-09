709a4dd283346af7aded119c7e9ef65282cc3b04
GaryHjy <782316959@qq.com>
2020-06-09 22:58:01 +0800

    feat: 2.注释

---
+++ 01.basic/02.注释.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// 1. 当行注释
+
+// 2. 多行注释
+/*
+  main是程序入口
+  每个程序都有这么一个函数
+  有且仅有一个
+*/
+
+int main() {
+
+  // 输出hello word
+  cout << "hello word" << endl;
+  system("pause");
+  return 0;
+}
