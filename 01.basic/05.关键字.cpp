35f2365cff485f39288d2670b2c82a520b5e76a6
GaryHjy <782316959@qq.com>
2020-06-09 23:52:31 +0800

    feat: 关键字

---
+++ 01.basic/05.关键字.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // cannot combine with previous 'int' declaration specifier
+  // 变量命名关键字会报错
+  // int int = 10;
+
+  cout << "hello word" << endl;
+
+  return 0;
+}
