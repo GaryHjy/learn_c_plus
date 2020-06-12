b34eb668a8822c2c86ca85fee74d47404244491b
GaryHjy <782316959@qq.com>
2020-06-12 10:26:51 +0800

    feat: 添加转义字符

---
+++ 02.数据类型/05.转义字符.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // 转义字符
+  
+  // 换行符 \n
+  cout << "hello word \n";
+
+  // 反斜杠 \\
+
+  cout << "hello word \\" << endl;
+
+  // 水平制表符 \t 作用是整理输出数据
+  cout << "aa\t\t\t hello word" << endl;
+  cout << "aaaaa\t\t hello word" << endl;
+  cout << "aaa\t\t\t hello word" << endl;
+
+  return 0;
+}
