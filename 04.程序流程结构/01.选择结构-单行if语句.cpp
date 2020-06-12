39bf18fd3c94780501dd34f443dd113bc26c1de4
GaryHjy <782316959@qq.com>
2020-06-12 17:02:22 +0800

    feat: 新增选择结构-单行if语句示例

---
+++ 04.程序流程结构/01.选择结构-单行if语句.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // 选择结构 单行if语句
+  // 用户输入分数，如果大于600，视为考入一本大学，在屏幕上输出
+
+  // 1.用户输入分数
+  int score = 0;
+  cout << "请输入一个分数：" << endl;
+  cin >> score;
+
+  // 2.打印用户输入的分数
+  cout << "您输入的分数为：" << score << endl;
+
+  // 3.判断分数是否大于600，如果大于，那么输出
+  // 注意事项：if后面不要带分号
+  if(score > 600) {
+    cout << "恭喜您考上了一本大学" << endl;
+  }
+
+  return 0;
+}
