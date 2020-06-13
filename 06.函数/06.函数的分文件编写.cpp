eee051ca3f79557e4572192fbaaf0f0339e93a4d
GaryHjy <782316959@qq.com>
2020-06-14 01:18:21 +0800

    feat: 添加函数的分文件编写示例

---
+++ 06.函数/06.函数的分文件编写.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// #include "./swap.h"
+using namespace std;
+
+// 函数的分文件编写
+// 1.创建后缀名为.h的头文件
+// 2.创建后缀名为.cpp的源文件
+// 3.在头文件写入函数的声明
+// 4.在源文件写函数的定义
+
+int main() {
+
+  // swap(100, 10);
+
+  return 0;
+}
