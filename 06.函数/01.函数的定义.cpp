a4b13fc62210cc400969bccc470ab08e30a1caa8
GaryHjy <782316959@qq.com>
2020-06-14 00:07:30 +0800

    feat: 添加函数的定义示例

---
+++ 06.函数/01.函数的定义.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// 语法
+// 返回值类型 函数名 (函数参数) {函数体语句 return表达式}
+
+// 加法函数，实现两个整型相加，并且将相加的结果返回
+int add(int num1, int num2) {
+  int sum = num1 + num2;
+  return sum;
+}
+
+int main() {
+  
+  return 0;
+}
